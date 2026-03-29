3ba8e5675ae4fc2e1d18e726fe0efdbac6ade1d1
Miguel <amiguelrubio27@gmail.com>
2026-03-29 21:55:50 +0200

    Set up Hilt dependency injection and initial Product List components
    - Create `MarketApp` class annotated with `@HiltAndroidApp`.
    - Annotate `MainActivity` with `@AndroidEntryPoint`.
    - Implement `ProductListViewModel` using the `@HiltViewModel` annotation.
    - Create `ProductListScreen` Composable with Hilt ViewModel injection.

---
+++ app/src/main/java/com/amrubio27/cursotestingandroid/productlist/presentation/ProductListScreen.kt
@@ -0,0 +1,13 @@
+package com.amrubio27.cursotestingandroid.productlist.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+
+@Composable
+fun ProductListScreen(
+    modifier: Modifier = Modifier,
+    productListViewModel: ProductListViewModel = hiltViewModel()
+) {
+
+}
