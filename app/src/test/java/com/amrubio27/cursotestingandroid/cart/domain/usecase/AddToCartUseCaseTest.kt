c09f87b5e5cc47553b7f057ca27a6e62740069f4
Miguel <amiguelrubio27@gmail.com>
2026-04-09 00:39:58 +0200

    Implement `ProductBuilder` and update `AddToCartUseCaseTest`
    - Create `ProductBuilder` DSL in the test suite to simplify `Product` model instantiation for unit tests.
    - Add test case to `AddToCartUseCaseTest` to verify that `InsufficientStock` error is thrown when requested quantity exceeds available stock.
    - Refactor existing tests in `AddToCartUseCaseTest` for better consistency and setup.

--- app/src/test/java/com/amrubio27/cursotestingandroid/cart/domain/usecase/AddToCartUseCaseTest.kt
+++ app/src/test/java/com/amrubio27/cursotestingandroid/cart/domain/usecase/AddToCartUseCaseTest.kt
@@ -1,8 +1,10 @@
 package com.amrubio27.cursotestingandroid.cart.domain.usecase
 
+import com.amrubio27.cursotestingandroid.core.builders.product
 import com.amrubio27.cursotestingandroid.core.domain.model.AppError
 import com.amrubio27.cursotestingandroid.core.fakes.FakeCartItemRepository
 import com.amrubio27.cursotestingandroid.core.fakes.FakeProductRepository
+import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
@@ -11,9 +13,7 @@ class AddToCartUseCaseTest {
     fun zero_quantity_throws_QuantityMustBePositive() = runTest {
         //Given
         val cartItemRepository = FakeCartItemRepository()
-        val productRepository = FakeProductRepository().apply {
-            setProducts(emptyList())
-        }
+        val productRepository = FakeProductRepository()
         val addToCartUseCase = AddToCartUseCase(cartItemRepository, productRepository)
 
         //When
@@ -41,13 +41,39 @@ class AddToCartUseCaseTest {
     fun non_existing_product_throws_NotFoundError() = runTest {
         //Given
         val cartItemRepository = FakeCartItemRepository()
-        val productRepository = FakeProductRepository()
-        val addToCartUseCase = AddToCartUseCase(cartItemRepository, productRepository)
+        val productRepository = FakeProductRepository().apply {
+            setProducts(emptyList())
+        }
+        val useCase = AddToCartUseCase(cartItemRepository, productRepository)
 
         //When
-        val exception = runCatching { addToCartUseCase("id", 1) }.exceptionOrNull()
+        val exception = runCatching { useCase("id", 1) }.exceptionOrNull()
 
         //Then
         assert(exception is AppError.NotFoundError)
+    }
+
+    @Test
+    fun insufficient_stock_throws_InsufficientStock() = runTest {
+        //Given
+        val productId = "id-test-1"
+        val product = product {
+            withId(productId)
+            withStock(2)
+        }
+        val cartItemRepository = FakeCartItemRepository()
+        val productRepository = FakeProductRepository().apply {
+            setProducts(listOf(product))
+        }
+        val useCase = AddToCartUseCase(cartItemRepository, productRepository)
+
+        //when
+        val exception = runCatching {
+            useCase(productId, 5)
+        }.exceptionOrNull()
+
+        //then
+        assert(exception is AppError.Validation.InsufficientStock)
+        assertEquals(2, (exception as AppError.Validation.InsufficientStock).available)
     }
 }
